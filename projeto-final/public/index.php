9921a9931c57ecff5f15f74c678af06176978ca6
Roner <ronerumb@gmail.com>
2022-06-13 19:54:12 -0300

    projeto final

---
+++ projeto-final/public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+include '../vendor/autoload.php';
+
+use App\Controller\IndexController;
+use App\Controller\ProductController;
+use App\Controller\ErrorController;
+
+$url = explode('?',$_SERVER['REQUEST_URI'])[0];
+
+function createRoute(string $controllerName, string $methodName) {
+    return [
+        'controller' => $controllerName,
+        'method' => $methodName,
+    ];
+}
+
+$routes = [
+  '/' => createRoute(IndexController::class, 'indexAction'),
+  '/produtos' => createRoute(ProductController::class,'listAction'),
+  '/produtos/novo' => createRoute(ProductController::class,'addAction'),
+];
+
+if (false === isset($routes[$url])) {
+    (new ErrorController())->notFoundAction();
+
+    exit;
+}
+$controllerName = $routes[$url]['controller'];
+$methodName = $routes[$url]['method'];
+
+(new $controllerName())->$methodName();
